25300c4309baf018a82feb0f587538bdaf460f11
DSY <2086930439@qq.com>
2023-07-03 15:13:25 +0800

    Demo02

---
+++ Basic Grammar/src/Demo02.java
@@ -0,0 +1,11 @@
+/**
+ * @Date 2023/7/3 15:10
+ **/
+public class Demo02 {
+    public static void main(String[] args) {
+        String a="hello";
+        int num=10;
+        System.out.println(a);
+        System.out.println(num);
+    }
+}

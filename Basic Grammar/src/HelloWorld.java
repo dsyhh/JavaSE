219568effcde1a627d755e09670851ee0e8b45a4
DSY <2086930439@qq.com>
2023-07-03 14:55:58 +0800

    HelloWorld

---
+++ Basic Grammar/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @Author DSY
+ * @Description Hello World
+ * @Date 2023/7/3 14:53
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
